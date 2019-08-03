a2effacf6d837b3c7e3f06f662117a641811d39f
Wolfgang Werner <mail@wolfgang-werner.net>
2019-08-03 21:39:18 +0200

    Add recursive binary search implementation

--- src/ch1/binary-search.ts
+++ src/ch1/binary-search.ts
@@ -1,19 +1,35 @@
-let list = Array.from(Array(1000).keys());
+let list = Array.from(Array(10000).keys());
 let steps = 0;
-console.log(binarySearch(453, list));
 
-function binarySearch(n: Number, list: Number[]): String | undefined {
-  let left = 0;
-  let right = list.length - 1;
+console.log('Incremental')
+console.log('-> ' + incrementalBinarySearch(37, list));
 
-  while (left <= right) {
+console.log('Recursive')
+steps = 0;
+console.log('-> ' + recursiveBinarySearch(37, list));
+
+function incrementalBinarySearch(n: Number, list: Number[]): Number | undefined {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
     console.log("Step " + steps++);
-    var mid = Math.floor((left + right) / 2);
+
+    var mid = Math.floor((low + high) / 2);
     var guess = list[mid];
 
-    if (guess == n) return "Found " + n + " at index " + mid;
-    if (guess > n) right = mid - 1;
-    if (guess < n) left = mid + 1;
+    if (guess == n) return n;
+    if (guess > n) high = mid - 1;
+    if (guess < n) low = mid + 1;
   }
   return undefined;
+}
+
+function recursiveBinarySearch(n: Number, list: Number[]) : Number | undefined{
+  console.log("Step " + steps++);
+
+  let mid = Math.floor(list.length/2)
+  if(list[mid] == n) return n
+  if(n > list[mid]) return recursiveBinarySearch(n, list.slice(mid+1, list.length));
+  if(n < list[mid]) return recursiveBinarySearch(n, list.slice(0, mid));
 }
